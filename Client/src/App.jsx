97befe1363225c74b35f3d3bcb4a0877ea887f54
shashanklko <shashankkm2003@gmail.com>
2026-05-17 16:29:58 +0530

    feat(frontend): initialize react router and global styles

---
+++ Client/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { Route, Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import { AuthProvider } from './context/AuthContext';
+import DashBoardLayout from './components/layout/DashboardLayout';
+import Dashboard from './pages/Dashboard';
+import AddStudent from './pages/AddStudent';
+import StudentDetails from './pages/StudentDetails';
+import SearchStudent from './pages/SearchStudent';
+import TransferStudent from './pages/TransferStudent';
+import CurrentStudents from './pages/CurrentStudents';
+import AddSchool from './pages/AddSchool';
+import ListedSchools from './pages/ListedSchools';
+
+const App = () => {
+    return (
+        <AuthProvider>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/track/:id" element={<div className="min-h-screen bg-gray-50 py-10 px-4"><StudentDetails /></div>} />
+                <Route path="/dashboard" element={<DashBoardLayout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path="addStudent" element={<AddStudent />} />
+                    <Route path="student/:id" element={<StudentDetails/>}/>
+                    <Route path="search" element={<SearchStudent />} />
+                    <Route path="transfer" element={<TransferStudent />} />
+                    <Route path="students" element={<CurrentStudents />} />
+                    <Route path="addSchool" element={<AddSchool />} />
+                    <Route path="schools" element={<ListedSchools />} />
+                </Route>
+        </Routes>
+        </AuthProvider >
+    );
+}
+export default App;
